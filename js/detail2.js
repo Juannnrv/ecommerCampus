f09e6dc3c63e1d621324385ee4441aa017f67fd7
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-14 08:27:36 -0500

    feat: :sparkles: Finalizamos la información y rating del producto
    Completamos la implementación para mostrar la información detallada y el rating del producto en detail.

--- js/detail2.js
+++ js/detail2.js
@@ -1,14 +1,20 @@
 
 import { galleryCategory } from "./components/gallery.js";
+import { titleProductDetail } from "./components/section.js";
 import { getProductById } from "./module/detail.js";
 
 let main__section_gallery = document.querySelector("#main__section_gallery");
+let main__section_tittle = document.querySelector("#main__section_tittle");
 
 addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
     let id = params.get('id');
     if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductById({id})));
-    main__section_gallery.innerHTML = await galleryCategory(JSON.parse(localStorage.getItem(id)))
+
+    let info = JSON.parse(localStorage.getItem(id));
+    main__section_gallery.innerHTML = await galleryCategory(info);
+    main__section_tittle.innerHTML = await titleProductDetail(info);
+
     // let {data} = res;
     // let {
     //     category_path,
