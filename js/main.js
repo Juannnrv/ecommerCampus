473fa68ceb1954db3eac5b6f0f074d273d86fd6f
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-14 08:58:01 -0500

    feat: :construction: Promesas para obtener el asin de los productos buscados

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 import { menuListCategoryIndex } from "./components/menu.js";
 import {galleryIndex} from "./components/gallery.js";
 import {getAllProductName, getAllCategory} from "./module/app.js";
+import { getProductById } from "./module/detail.js";
 
 let input__search = document.querySelector("#input__search");
 let main__article = document.querySelector(".main__article");
@@ -15,9 +16,22 @@ addEventListener("DOMContentLoaded", async e=>{
 
 input__search.addEventListener("change", async e => {
   let params = new URLSearchParams(location.search);
-  let data = { search : e.target.value, id: params.get('id')}
+  let datasearch = { search : e.target.value, id: params.get('id')}
   input__search.value = null;
 
-  let res = await getAllProductName(data)
+  let res = await getAllProductName(datasearch)
   main__article.innerHTML = galleryIndex(res, params.get('id'));
-});
+
+  // let { data : {products} } = res;
+  // let asin = products.map(value => {return {id : value.asin}});
+  
+  // let process = new Promise((resolve, reject) => {
+  //   for (let i = 0; i < asin.length; i++) {
+  //     if (localStorage.getItem(asin[i].id)) continue;
+  //     let data = await getProductById(asin[i]);
+  //     localStorage.setItem(asin[i].id, JSON.stringify(data));
+  //   }
+  //   resolve({message: "Data found succesfully"})
+  //   })
+  //   Promise.race([process]).then(res => {console.log(res);})
+  });
