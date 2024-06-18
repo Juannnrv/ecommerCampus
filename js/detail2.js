ad6e6b1845571280faaf25c4a8ce9638f07a2fd9
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-18 08:20:13 -0500

    feat: :art: KISS y validación en la descripción del producto en caso de que sea null
    Aplicamos KISS y una validación en caso de null el evento del read more de descripción del producto

--- js/detail2.js
+++ js/detail2.js
@@ -14,68 +14,68 @@ addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
     let id = params.get('id');
     if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductById({id})));
-
+    
     let info = JSON.parse(localStorage.getItem(id));
     main__section_gallery.innerHTML = await galleryCategory(info);
     main__section_tittle.innerHTML = await titleProductDetail(info);
     main__section_description.innerHTML = await descriptionProductDetail(info);
     main__section_custom.innerHTML = await sizeProductDetail(info);
     footer__ul_cost.innerHTML = await valueProductDetail(info);
-
+    
     // let {data} = res;
     // let {
-    //     category_path,
-    //     about_product,
-    //     product_details,
-    //     product_information,
-    //     product_photos,
-    //     product_variations,
-    //     rating_distribution,
-    //     review_aspects,
-    //     ...dataUpdate
-    // } = data;
-    // console.log(dataUpdate);
+        //     category_path,
+        //     about_product,
+        //     product_details,
+        //     product_information,
+        //     product_photos,
+        //     product_variations,
+        //     rating_distribution,
+        //     review_aspects,
+        //     ...dataUpdate
+        // } = data;
+        // console.log(dataUpdate);
+        
+        let s = document.querySelector("#s")
+        let m = document.querySelector("#m")
+        let l = document.querySelector("#l")
+        let xl = document.querySelector("#xl")
+        
+        s.addEventListener("click", (e) => {
+            s.src="../storage/img/snuevo.svg"
+            m.src="../storage/img/m.svg"
+            l.src="../storage/img/l.svg"
+            xl.src="../storage/img/xl.svg"
+        })
+        m.addEventListener("click", (e) => {
+            m.src="../storage/img/mnuevo.svg"
+            s.src="../storage/img/s.svg"
+            l.src="../storage/img/l.svg"
+            xl.src="../storage/img/xl.svg"
+        })
+        l.addEventListener("click", (e) => {
+            l.src="../storage/img/lnuevo.svg"
+            s.src="../storage/img/s.svg"
+            m.src="../storage/img/m.svg"
+            xl.src="../storage/img/xl.svg"
+        })
+        xl.addEventListener("click", (e) => {
+            xl.src="../storage/img/xlnuevo.svg"
+            s.src="../storage/img/s.svg"
+            m.src="../storage/img/m.svg"
+            l.src="../storage/img/l.svg"
+        })
+        
+        let menos = document.querySelector("#menos");
+        let num = document.querySelector("#num");
+        let mas = document.querySelector("#mas");
+        let precio = document.querySelector("#precio")
 
-    let s = document.querySelector("#s")
-    let m = document.querySelector("#m")
-    let l = document.querySelector("#l")
-    let xl = document.querySelector("#xl")
-
-    s.addEventListener("click", (e) => {
-        s.src="../storage/img/snuevo.svg"
-        m.src="../storage/img/m.svg"
-        l.src="../storage/img/l.svg"
-        xl.src="../storage/img/xl.svg"
-    })
-    m.addEventListener("click", (e) => {
-        m.src="../storage/img/mnuevo.svg"
-        s.src="../storage/img/s.svg"
-        l.src="../storage/img/l.svg"
-        xl.src="../storage/img/xl.svg"
-    })
-    l.addEventListener("click", (e) => {
-        l.src="../storage/img/lnuevo.svg"
-        s.src="../storage/img/s.svg"
-        m.src="../storage/img/m.svg"
-        xl.src="../storage/img/xl.svg"
-    })
-    xl.addEventListener("click", (e) => {
-        xl.src="../storage/img/xlnuevo.svg"
-        s.src="../storage/img/s.svg"
-        m.src="../storage/img/m.svg"
-        l.src="../storage/img/l.svg"
-    })
-
-    let menos = document.querySelector("#menos");
-    let num = document.querySelector("#num");
-    let mas = document.querySelector("#mas");
-    let precio = document.querySelector("#precio")
-
-    menos.addEventListener("click", (e) => {
-        if(num.textContent > 1) num.textContent = parseInt(num.textContent) - 1;
-        let productPrice = info.data.product_price ? parseFloat(info.data.product_price.replace("$", "")) : 0;
-        let productPriceOriginal = info.data.product_original_price ? parseFloat(info.data.product_original_price.replace("$", "")) : 0;
-        let totalPrice = productPrice * parseInt(num.textContent);
+        menos.addEventListener("click", (e) => {
+            if(num.textContent > 1) num.textContent = parseInt(num.textContent) - 1;
+            let productPrice = info.data.product_price ? parseFloat(info.data.product_price.replace("$", "")) : 0;
+            let productPriceOriginal = info.data.product_original_price ? parseFloat(info.data.product_original_price.replace("$", "")) : 0;
+            let totalPrice = productPrice * parseInt(num.textContent);
         let totalPriceOriginal = productPriceOriginal * parseInt(num.textContent);
         console.log(totalPriceOriginal);
         
@@ -84,7 +84,7 @@ addEventListener("DOMContentLoaded", async(e)=>{
         precio.innerHTML = /*html*/`
         <span id="precio" >Add to Cart | $${totalPrice.toFixed(2)} <sub>${originalPriceDisplay}</sub></span>`;
     });
-
+    
     mas.addEventListener("click", (e) => {
         num.textContent = parseInt(num.textContent) + 1;
         let productPrice = info.data.product_price ? parseFloat(info.data.product_price.replace("$", "")) : 0;
@@ -102,12 +102,9 @@ addEventListener("DOMContentLoaded", async(e)=>{
     
     let strong_description = document.querySelector("#text");
     let parrafo = document.querySelector("#parrafo"); 
-    
-    strong_description.addEventListener("click", (e) => {
-        let completed =  info.data.product_description;
-        parrafo.textContent = completed;
-    });
 
+    strong_description ? strong_description.addEventListener("click", () => parrafo.textContent = info.data.product_description || "") : parrafo;
+    }
 
-})
+)
 
