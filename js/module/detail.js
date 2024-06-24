f2d49ff52c467a032d57ef556ef2fc83615c0e42
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-24 17:06:07 -0500

    release: :bookmark: Página Wishlist
    feat: :sparkles: Wishlist
    
    Agregamos el apartado de la Wishlist de todos los productos seleccionados
    
    hash => c75583f4e9aad2de9f99611d15ec7a24f65a923e

--- js/module/detail.js
+++ js/module/detail.js
@@ -1,6 +1,7 @@
 
 import { headers } from "../components/env.js";
 
+document.querySelector(".carga").style.display = "block";
 export const getProductById = async({id:idCategory})=>{
     console.log("Waiting...");
     const url = `https://real-time-amazon-data.p.rapidapi.com/product-details?asin=${idCategory}&country=US`;
@@ -10,5 +11,8 @@ export const getProductById = async({id:idCategory})=>{
     };
     let res = await fetch(url, options);
     let data = res.json();
+
+    document.querySelector(".carga").style.display = "none";
+
     return data;
 }
