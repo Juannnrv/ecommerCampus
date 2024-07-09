7dc68645de169deac10eff43476995ed682ae8a2
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-07-09 07:06:02 -0500

    chore: :truck: Endpoint actualizado de la API de Amazon
    Se ha actualizado el endpoint para realizar peticiones a la API

--- js/components/env.js
+++ js/components/env.js
@@ -1,5 +1,5 @@
 
 export const headers =  {
-    'x-rapidapi-key': '61ebabc795msh16b7c12b22e86adp151f79jsn6528c9dac61e',
+    'x-rapidapi-key': 'f340b75677msh78390a7ac842696p15076ajsne59e4d1f2402',
     'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
